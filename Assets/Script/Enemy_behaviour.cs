daf2ac837148e7d5a71554e55a13906615167ff6
05AliAhmed <20ali0504@gmail.com>
2025-11-29 14:05:59 +0800

    Enemy attacking

--- Assets/Script/Enemy_behaviour.cs
+++ Assets/Script/Enemy_behaviour.cs
@@ -9,7 +9,7 @@ public class Enemy_behaviour : MonoBehaviour
     [SerializeField] private float collisionDistance;
     [SerializeField] private int damage;
     [SerializeField] private BoxCollider2D boxCollider;
-    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private LayerMask playerLayer;
     private float cooldowntimer = Mathf.Infinity;
 
     private Animator anim;
@@ -24,19 +24,24 @@ public class Enemy_behaviour : MonoBehaviour
         cooldowntimer += Time.deltaTime;
 
         // Attack only when player insight
-
-        if(cooldowntimer > attackcooldown)
+        if(playerInsight())
         {
-            cooldowntimer = 0;
-            anim.SetTrigger("attack"); 
-        }    
+            if(cooldowntimer > attackcooldown)
+            {
+                cooldowntimer = 0;
+                anim.SetTrigger("attack"); 
+            }       
+        }
+         
     }
 
     private bool playerInsight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * collisionDistance, 
+        RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * collisionDistance, 
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-            0, Vector2.left, 0, enemyLayer);
+            0, Vector2.left, 0, playerLayer);
+
         return hit.collider != null;
     }
 
@@ -46,5 +51,38 @@ public class Enemy_behaviour : MonoBehaviour
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * collisionDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z ));
     }
+
+    // private bool playerInsight()
+    // {
+    //     // Determine cast direction based on enemy facing
+    //     Vector2 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    //     // Cast box forward
+    //     RaycastHit2D hit = Physics2D.BoxCast(
+    //         boxCollider.bounds.center,
+    //         new Vector2(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y),
+    //         0f,
+    //         direction,
+    //         collisionDistance,
+    //         playerLayer
+    //     );
+
+    //     return hit.collider != null;
+    // }
+
+    // private void OnDrawGizmos()
+    // {
+    //     if (boxCollider == null) return;
+
+    //     Gizmos.color = Color.red;
+    //     Vector2 direction = transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    //     Gizmos.DrawWireCube(
+    //         boxCollider.bounds.center + (Vector3)(direction * collisionDistance),
+    //         new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, 1)
+    //     );
+    // }
+
+
 }
 
