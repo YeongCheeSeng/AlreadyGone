cb9b3dd8453e0a13bc59a0b5b550a37d7142119e
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-11-29 20:38:17 +0800

    hand movement added

--- Assets/Script/Hand/HandMovement.cs
+++ Assets/Script/Hand/HandMovement.cs
@@ -8,18 +8,45 @@ public class HandMovement : MonoBehaviour
     [SerializeField] private float speed = 1.0f;
 
     private Rigidbody2D rb;
+    private Vector3 originalTransform;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalTransform = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (target == null || rb == null) return;
-        
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+
+        //Movement Towards Target
+        rb.velocity = (target.transform.position - transform.position).normalized * speed;
+
+        //Sprite Flipping
+        if (rb.velocity.x > 0.1f)
+        {
+            transform.localScale = new Vector3(-originalTransform.x, originalTransform.y, originalTransform.z);
+        }
+        else if (rb.velocity.x < -0.1f)
+        {
+            transform.localScale = new Vector3(originalTransform.x, originalTransform.y, originalTransform.z);
+        }
+
+        //Animation Handling
+        if (animator != null)
+        {
+            if (rb.velocity.magnitude > 0.1f)
+            {
+                animator.Play("Hand_Walk");
+            }
+            else
+            {
+                animator.Play("Hand_Idle");
+            }
+        }
     }
 }
