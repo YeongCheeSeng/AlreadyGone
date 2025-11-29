d0e15f1f0d620ba584ca8b8265730c62fbb3ffcf
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-11-29 15:46:20 +0800

    thunder & rain sound effect added

--- Assets/Script/Light/LightFollowTarget.cs
+++ Assets/Script/Light/LightFollowTarget.cs
@@ -7,9 +7,12 @@ public class LightFollowTarget : MonoBehaviour
 {
     public GameObject target;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = target.transform.position - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
