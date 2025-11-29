9b99c909fe057cb6c242de315cb5de4f41521e40
05AliAhmed <20ali0504@gmail.com>
2025-11-29 21:01:54 +0800

    working

--- Assets/Script/EnemyFollow.cs
+++ Assets/Script/EnemyFollow.cs
@@ -69,9 +69,9 @@ public class EnemyFollow : MonoBehaviour
         rb.velocity = new Vector2(direction * speed, rb.velocity.y);
 
     if (direction > 0)
-        enemy.transform.localScale = new Vector3(2, 2, 2);
+        enemy.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
     else
-        enemy.transform.localScale = new Vector3(-2, 2, 2);  
+        enemy.transform.localScale = new Vector3(-2.5f, 2.5f, 2.5f);  
 
     }
 }
