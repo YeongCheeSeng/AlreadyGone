beba5e699aa46c68cf1bedc1c875a37f362b0589
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-11-29 17:32:20 +0800

    player ui fixed

--- Assets/Script/UI/Player_UiManager.cs
+++ Assets/Script/UI/Player_UiManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
-    private GameObject player;
+    private PlayerMovement playerMovement;
     public GameObject bar;
     
-
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        playerMovement = FindObjectOfType<PlayerMovement>();
     }
 
-    void Update()
+    void LateUpdate()
     {
-        
+        if (playerMovement == null) return;
+
+        if (playerMovement.isFacingRight)
+            bar.transform.localScale = new Vector3(Mathf.Abs(bar.transform.localScale.x), bar.transform.localScale.y);
+        else
+            bar.transform.localScale = new Vector3(-Mathf.Abs(bar.transform.localScale.x), bar.transform.localScale.y);
     }
 }
