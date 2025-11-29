392c27bd34421e6eb071e7ce46d7b67edfb7f056
Tan Kenji <kenjitan1020@gmail.com>
2025-11-29 16:30:37 +0800

    Finished AutoMove, WIP cinematic bar
    - AutoMove now works as intended
    - Right now haven't started on cinematic bar system

--- Assets/Script/Cinematic & AutoMove/EndPointZone.cs
+++ Assets/Script/Cinematic & AutoMove/EndPointZone.cs
@@ -6,6 +6,7 @@ public class EndPointZone : MonoBehaviour
 {
     [SerializeField] private float moveDuration = 2.8f; // Duration for moving the player
     [SerializeField] private float waitBeforeLoad = 5f; // Wait time before loading next scene
+    public bool walkingInStart = true;
     Collider2D endGameZone;
     private PlayerMovement player;
     private Rigidbody2D playerRigidbody;
@@ -27,30 +28,46 @@ public class EndPointZone : MonoBehaviour
                 playerRigidbody = target.GetComponent<Rigidbody2D>();
                 
                 // Disable player controls
-                StartCoroutine(VictorySequence());
+                player.SetCanMove(false);
+                StartCoroutine(AutoMove());
                 endGameZone.enabled = false; // Disable the collider to prevent re-triggering
             }
         }
     }
 
-    private IEnumerator VictorySequence()
+    private IEnumerator AutoMove()
     {
         float elapsedTime = 0f;
         Vector2 originalVelocity = playerRigidbody.velocity;
+
+        // Play walk animation once
+        player.animator.Play("Player_Walk");
 
         // Move player to the right for moveDuration seconds
         while (elapsedTime < moveDuration)
         {
             playerRigidbody.velocity = new Vector2(player.speed, originalVelocity.y);
-            //player.HandleAnimations(); // Update animations if necessary
+            
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        // Wait for seconds before loading next scene
-        yield return new WaitForSeconds(waitBeforeLoad);
+        // Stop movement
+        playerRigidbody.velocity = new Vector2(0, originalVelocity.y);
+        player.animator.Play("Player_Idle");
 
-        // Put loading next scene here
-        Debug.Log("Loading Next Scene");
+        if (walkingInStart)
+        {
+            player.SetCanMove(true);
+        }
+        else
+        {
+            // Wait for seconds before loading next scene
+            yield return new WaitForSeconds(waitBeforeLoad);
+
+            // Load the next scene
+            //SceneManager.LoadScene("Ending");
+            Debug.Log("Loading Ending Scene...");
+        }
     }
 }
